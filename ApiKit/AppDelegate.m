2e7d4fcdcb9f35ae8a75f6e853d5885cab1589bd
johannkerr <johannkerr@gmail.com>
2015-08-12 14:25:54 -0400

    Update AppDelegate.m

--- ApiKit/AppDelegate.m
+++ ApiKit/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+//Johann was here
     // Override point for customization after application launch.
     return YES;
 }
